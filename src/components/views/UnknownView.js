32ddbe53f7ff007d246dcf00e435c1a3e06225f3
Clement G <clement@garrot.org>
2019-05-22 23:13:50 +0200

    create view component UnknownView (for 404 route)

---
+++ src/components/views/UnknownView.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class UnknownView extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        return(
+            <div >
+                <h1>View : NOT FOUND !</h1>
+            </div>
+        )
+    }
+}
+export default UnknownView;
