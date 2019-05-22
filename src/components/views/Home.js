57f0309465f2e868a269b52fd152b1f3cb7d3ad2
Clement G <clement@garrot.org>
2019-05-22 23:11:01 +0200

    create View Component Home

---
+++ src/components/views/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        return(
+            <div >
+               <h1>View : HOME</h1>
+            </div>
+        )
+    }
+}
+export default Home;
