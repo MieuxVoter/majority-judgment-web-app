bd0056b54ea80059d2af78a35d7b92d901e12683
Clement G <clement@garrot.org>
2019-05-22 23:13:13 +0200

    create view component CreateBallot

---
+++ src/components/views/CreateBallot.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class CreateBallot extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        return(
+            <div >
+               <h1>View : CREATE BALLOT</h1>
+            </div>
+        )
+    }
+}
+export default CreateBallot;
