d4d54777bb52c3277fa8ed15d1f9006a7645e548
Clement G <clement@garrot.org>
2019-06-08 15:02:22 +0200

    Add Header and Footer component layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import Routes from "./Routes.js";
 import './App.css';
 
-import Home from "./components/views/Home.js";
-import CreateBallot from "./components/views/CreateBallot.js";
-import UnknownView from "./components/views/UnknownView";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
 
 function App() {
   return (
       <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/create-ballot">Créer un vote</Link>
-            </li>
-          </ul>
-          <hr />
-	  <Routes />
+            <Header />
+	        <Routes />
+            <Footer />
         </div>
       </Router>
   );
---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Footer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        return(
+            <div >
+                <h1>FOOTER</h1>
+            </div>
+        )
+    }
+}
+export default Footer;
