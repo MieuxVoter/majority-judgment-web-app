1c2c3fdaa70c0fa43ca918c5bf033565ff38782a
Clement G <clement@garrot.org>
2019-06-08 18:48:45 +0200

    Add first form on Home page

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,4 +1,7 @@
 import React, {Component} from "react";
+import { Container, Row, Col,Button, Input  } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRocket } from '@fortawesome/free-solid-svg-icons'
 
 class Home extends Component {
 
@@ -10,9 +13,25 @@ class Home extends Component {
 
     render(){
         return(
-            <div >
-               <h1>View : HOME</h1>
-            </div>
+            <Container>
+                <Row>
+                    <Col className="text-center"><h3>Simple, gratuit : organisez un vote à l'aide du Jugement Majoritaire.</h3></Col>
+                </Row>
+                <Row className="mt-2">
+
+                    <Col xs="12" md="9" xl="6" className="offset-xl-2">
+                            <Input placeholder="Question de votre vote" className="mt-2" />
+                    </Col>
+
+                    <Col xs="12" md="3" xl="2">
+                        <Button addonType="append" className="btn btn-block btn-secondary mt-2"><FontAwesomeIcon icon={faRocket} className="mr-2"/>Lancer</Button>
+                    </Col>
+                </Row>
+                <Row className="mt-4">
+                    <Col className="text-center"><p>Pas de publicité et pas de cookie publicitaire.</p></Col>
+                </Row>
+            </Container>
+
         )
     }
 }
