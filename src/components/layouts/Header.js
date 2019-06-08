824ee09b7f1f13767719ef7f6f9bf0f2ce52bcb2
Clement G <clement@garrot.org>
2019-06-08 15:02:22 +0200

    Add Header and Footer component layout

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        return(
+            <div >
+                <h1>HEADER</h1>
+            </div>
+        )
+    }
+}
+export default Header;
