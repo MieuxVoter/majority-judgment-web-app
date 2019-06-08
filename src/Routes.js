81b07f012199c07e8ea35398dbaaf0da31ed11ba
Clement G <clement@garrot.org>
2019-06-08 14:52:10 +0200

    Add Vote and Result view component (with Routes)

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,8 @@ import {Switch, Route } from "react-router-dom";
 
 import Home from "./components/views/Home.js";
 import CreateBallot from "./components/views/CreateBallot.js";
+import Vote from "./components/views/Vote";
+import Result from "./components/views/Result";
 import UnknownView from "./components/views/UnknownView";
 
 function Routes() {
@@ -10,6 +12,8 @@ function Routes() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/create-ballot" component={CreateBallot} />
+            <Route path="/vote" component={Vote} />
+            <Route path="/result" component={Result} />
             <Route component={UnknownView} />
           </Switch>
   );
---
+++ src/components/views/Vote.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Vote extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        return(
+            <div >
+               <h1>View : Vote</h1>
+            </div>
+        )
+    }
+}
+export default Vote;
