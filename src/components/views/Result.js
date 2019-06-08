2466adf05a2282f091b46093a80aa2e32f6d868b
Clement G <clement@garrot.org>
2019-06-08 14:52:10 +0200

    Add Vote and Result view component (with Routes)

---
+++ src/components/views/Result.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Result extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        return(
+            <div >
+               <h1>View : RESULT</h1>
+            </div>
+        )
+    }
+}
+export default Result;
