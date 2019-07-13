cb8ca2c7103604555f3ff6d80719f0873035bcd4
Clement G <clement@garrot.org>
2019-07-13 11:25:21 +0200

    Remove unused vars

--- src/components/views/Vote.js
+++ src/components/views/Vote.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import {Button, Col, Container, Row} from "reactstrap";
 import {toast, ToastContainer} from "react-toastify";
-import ButtonWithConfirm from "../form/ButtonWithConfirm";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCheck} from "@fortawesome/free-solid-svg-icons";
 
