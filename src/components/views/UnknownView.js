f0fab636843383429b98175410de654171943d02
Clement G <clement@garrot.org>
2019-09-14 22:49:57 +0200

    feat(views): Enhanced view for 404 error

--- src/components/views/UnknownView.js
+++ src/components/views/UnknownView.js
@@ -1,4 +1,8 @@
 import React, {Component} from "react";
+import {Button, Col, Container,  Row} from "reactstrap";
+import logoLine from "../../logos/logo-line-white.svg";
+import {Link} from 'react-router-dom';
+
 
 class UnknownView extends Component {
 
@@ -10,9 +14,19 @@ class UnknownView extends Component {
 
     render(){
         return(
-            <div >
-                <h1>View : NOT FOUND !</h1>
-            </div>
+            <Container>
+                    <Row>
+                        <Link  to="/" className="d-block ml-auto mr-auto mb-4"><img src={logoLine} alt="logo" height="128" /></Link>
+                    </Row>
+                    <Row className="mt-4">
+                        <Col className="text-center"><h2>Oups ! Cette page n'existe pas</h2></Col>
+                    </Row>
+                    <Row className="mt-4" >
+                        <Col className="text-center">
+                            <Button to="/" className="btn btn-primary">Revenir à l'accueil</Button>
+                        </Col>
+                    </Row>
+            </Container>
         )
     }
 }
