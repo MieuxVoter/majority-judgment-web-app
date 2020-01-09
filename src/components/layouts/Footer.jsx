e2fa5e2b95dc66a7427f56622c05bd25110e5c07
Adel Zhr <AdelZhr@MacBook-Air-de-Adel-3.local>
2020-01-09 13:45:04 +0100

    Add the sponsor badge in the Footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import logo from "../../logos/helloasso-logo.png";
 
 class Footer extends Component {
   constructor(props) {
@@ -15,6 +16,13 @@ class Footer extends Component {
         <a href="https://github.com/MieuxVoter">Code source</a>
         <span className="m-2">-</span>
         <a href="https://mieuxvoter.fr/">Qui sommes nous ?</a>
+	<a href="https://www.helloasso.com/associations/mieux-voter">
+            <div className="mt-2">
+              <div className="align-self-center">
+                <img src={logo} alt="logo" height="32" />
+              </div>
+            </div>
+        </a>
         <div className="mt-2">MieuxVoter &copy;</div>
       </footer>
     );
