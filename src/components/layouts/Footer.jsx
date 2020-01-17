744fe62c5ae150cb789bc32b332c78f146714817
Adel Zhr <AdelZhr@MacBook-Air-de-Adel-3.local>
2020-01-17 20:54:21 +0100

    add target=blank in the Footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -13,10 +13,10 @@ class Footer extends Component {
       <footer className="text-center">
       	  <Link to="/">Accueil</Link>
           <span className="m-2">-</span>
-          <a href="https://github.com/MieuxVoter">Code source</a>
+          <a href="https://github.com/MieuxVoter" target="_blank">Code source</a>
           <span className="m-2">-</span>
-          <a href="https://mieuxvoter.fr/">Qui sommes nous ?</a>
-          <a href="https://www.helloasso.com/associations/mieux-voter" >
+          <a href="https://mieuxvoter.fr/" target="_blank">Qui sommes nous ?</a>
+          <a href="https://www.helloasso.com/associations/mieux-voter" target="_blank">
             <div className="mt-2">
               <div className="align-self-center">
               <h5>Faire un don :     <img src={logo} alt="logo" height="28" />
