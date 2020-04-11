6a7b57f754c78bf403b7fe271941f3259c1e1dfa
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2020-04-11 18:20:19 +0200

    add spinner and suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Routes from "./Routes";
+import React, {Suspense} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-import Header from "./components/layouts/Header";
-import Footer from "./components/layouts/Footer";
-import AppContextProvider from "./AppContext";
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
+
+import Routes from './Routes';
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import AppContextProvider from './AppContext';
+import {i18n} from './i18n';
+
+const Spinner = (
+  <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={1000} />
+);
 
 function App() {
   return (
+    <Suspense fallback={Spinner}>
       <AppContextProvider>
         <Router>
           <div>
@@ -17,6 +26,7 @@ function App() {
           </div>
         </Router>
       </AppContextProvider>
+    </Suspense>
   );
 }
 
---
+++ src/i18n.jsx
@@ -0,0 +1,34 @@
+import i18n from 'i18next';
+import XHR from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+
+i18n
+  .use(XHR)
+  .use(LanguageDetector)
+  .use(initReactI18next) // bind react-i18next to the instance
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react!!
+    },
+
+    // react i18next special options (optional)
+    // override if needed - omit if ok with defaults
+    /*
+    react: {
+      bindI18n: 'languageChanged',
+      bindI18nStore: '',
+      transEmptyNodeValue: '',
+      transSupportBasicHtmlNodes: true,
+      transKeepBasicHtmlNodesFor: ['br', 'strong', 'i'],
+      useSuspense: true,
+    }
+    */
+  });
+
+
+export default i18n;
