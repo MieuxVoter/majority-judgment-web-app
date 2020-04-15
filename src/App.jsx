f3cc5d77c1a28ead2d3429e687c90faefed2420f
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2020-04-15 19:36:38 +0200

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, {Suspense} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import Loader from 'react-loader-spinner';
+import Loader from './components/loader';
 
 import Routes from './Routes';
 import Header from './components/layouts/Header';
@@ -10,13 +10,13 @@ import Footer from './components/layouts/Footer';
 import AppContextProvider from './AppContext';
 import {i18n} from './i18n';
 
-const Spinner = (
-  <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={1000} />
-);
+// const Spinner = (
+//   <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={1000} />
+// );
 
 function App() {
   return (
-    <Suspense fallback={Spinner}>
+    <Suspense fallback={Loader}>
       <AppContextProvider>
         <Router>
           <div>
