edc20ddba49190eafd62d431f91f1193086071d0
Clement G <clement@garrot.org>
2020-04-16 19:54:31 +0200

    feat(loader): modify layout, blue background, with smaller and centered logo

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './loader-pulse-2-alpha.gif';
+import logo from './loader-pulse-2.gif';
 import './style.css';
 
 const Loader = () => {
 return (
-<div className="loader">
+<div className="loader bg-primary">
 	<img src={logo} alt="Loading..." />
 </div>);
 };
--- src/components/wait/index.jsx
+++ src/components/wait/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import Loader from '../loader';
-import './style.scss';
 
 const Wait = () => {
-return (
-<div className="waiting">
-	<Loader />
-</div>);
+return (<Loader />);
 };
 
 
