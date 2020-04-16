c125b2a7081d7206c405abe95d671f57ac65da4e
Clement G <clement@garrot.org>
2020-04-16 19:37:09 +0200

    style(flag): Apply prettier

--- src/components/flag.js
+++ src/components/flag.js
@@ -1,5 +1,4 @@
-import * as React from 'react' 
-import FlagIconFactory from 'react-flag-icon-css'
+import * as React from "react";
+import FlagIconFactory from "react-flag-icon-css";
 
-	
-export  const FlagIcon = FlagIconFactory(React, { useCssModules: false })
+export const FlagIcon = FlagIconFactory(React, { useCssModules: false });
