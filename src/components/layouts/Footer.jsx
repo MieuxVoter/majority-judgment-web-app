8864895d64a674cc284faec87922f0dfd9afadfd
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2020-04-19 17:22:10 +0000

    remove flags from footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,51 +1,42 @@
-import React, { Component, Fragment } from "react";
+import React, {Component} from 'react';
 import {withTranslation} from 'react-i18next';
-import { Link } from "react-router-dom";
-import { FlagIcon } from '../flag';
-import Paypal from "../banner/Paypal";
-import i18n from '../../i18n'
+import {Link} from 'react-router-dom';
+import Paypal from '../banner/Paypal';
 
 class Footer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
-
-
   render() {
-
-  const linkStyle = {whiteSpace: "nowrap"};
-  const {t} = this.props;
-  const buttonStyle = {backgroundColor: "black", padding: "0px", border: "0px",};
-  const countries = [
-    {"l": "en", "flag": "gb"},
-    {"l": "es", "flag": "es"},
-    {"l": "fr", "flag": "fr"},
-    {"l": "de", "flag": "de"},
-  ];
+    const linkStyle = {whiteSpace: 'nowrap'};
+    const {t} = this.props;
     return (
       <footer className="text-center">
-        <Link to="/" style={linkStyle}>{t("Homepage")}</Link>
+        <Link to="/" style={linkStyle}>
+          {t('Homepage')}
+        </Link>
         <span className="m-2">-</span>
-        <a href="https://github.com/MieuxVoter" target="_blank" rel="noopener noreferrer" style={linkStyle}>{t("Source code")}</a>
+        <a
+          href="https://github.com/MieuxVoter"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={linkStyle}>
+          {t('Source code')}
+        </a>
         <span className="m-2">-</span>
-        <a href="https://mieuxvoter.fr/" target="_blank" rel="noopener noreferrer" style={linkStyle} >{t("Who are we?")}</a>
+        <a
+          href="https://mieuxvoter.fr/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={linkStyle}>
+          {t('Who are we?')}
+        </a>
         <span className="m-2">-</span>
-        {
-        countries.map(({l, flag}, i) => (
-        <Fragment key={i} >
-          <button style={buttonStyle} onClick={() => i18n.changeLanguage(l)}>
-            <FlagIcon code={flag}  />
-          </button>
-            {" "}
-            </Fragment>
-        ))
-        }
-          <div className="mt-3">
-              <Paypal btnColor="btn-primary"/>
-          </div>
+        <div className="mt-3">
+          <Paypal btnColor="btn-primary" />
+        </div>
       </footer>
     );
   }
