38c32e4e3c61415c888579b8dd3728f0f67e79d7
Clement G <clement@garrot.org>
2020-04-19 13:21:11 +0200

    feat(paypal): add color property (with class bootstrap) #31

--- src/components/banner/Paypal.jsx
+++ src/components/banner/Paypal.jsx
@@ -18,7 +18,7 @@ const Paypal = (props) => {
     return (
         <div className="d-inline-block m-auto">
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <button type="submit" className="btn btn-primary"  title={t("PayPal - The safer, easier way to pay online!")}  > <FontAwesomeIcon icon={faPaypal} className="mr-2" />{t("Support us !")}</button>
+            <button type="submit" className={"btn "+props.btnColor}  title={t("PayPal - The safer, easier way to pay online!")}  > <FontAwesomeIcon icon={faPaypal} className="mr-2" />{t("Support us !")}</button>
             <input type="hidden" name="cmd" value="_s-xclick" />
             <input type="hidden" name="hosted_button_id" value="KB2Z7L9KARS7C" />
             <img alt="" border="0" src={pixelLink} width="1" height="1" />
