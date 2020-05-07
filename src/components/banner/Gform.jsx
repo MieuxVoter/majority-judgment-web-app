13e165dc7cbf89503fb8ff014f8ea4b47864a5fa
guhur <12297742+guhur@users.noreply.github.com>
2020-05-07 20:40:19 +0200

    fix(gform): switch to Component

--- src/components/banner/Gform.jsx
+++ src/components/banner/Gform.jsx
@@ -1,20 +1,22 @@
 /* eslint react/prop-types: 0 */
-import React from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogleDrive } from "@fortawesome/free-brands-svg-icons";
 
-const Gform = props => {
-  return (
-    <a
-      className={props.className}
-      href={this.context.feedbackForm}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <FontAwesomeIcon icon={faGoogleDrive} className="mr-2" />
-      Votre avis nous intéresse !
-    </a>
-  );
+class Gform extends Component {
+  render () {
+    return (
+      <a
+        className={this.props.className}
+        href={this.context.feedbackForm}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGoogleDrive} className="mr-2" />
+        Votre avis nous intéresse !
+      </a>
+    );
+  }
 };
 
 export default Gform;
