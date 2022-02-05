ba0ae5dfc8c7167fc19191f7097994e9928841e4
nicgirault <nic.girault@gmail.com>
2022-02-05 21:42:43 +0100

    init cra

--- next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: "fr",
-    locales: ["en", "fr", "de", "es", "ru"],
-    ns: ["resource", "common", "error"],
-    defaultNS: "resource",
-    fallbackNS: ["common", "error"],
-  },
-};
