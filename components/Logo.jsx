56836e5ea1d860829dad78f13c20c4cd0cfed1b2
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-10-27 16:48:10 +0200

    fix: clean up heomapge

---
+++ components/Logo.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import logoWithText from '../public/logos/logo.svg'
+import logo from '../public/logos/logo-footer.svg'
+import {useTranslation} from "next-i18next";
+
+export const getStaticProps = async ({locale}) => ({
+  props: {
+    ...(await serverSideTranslations(locale, [], config)),
+  },
+});
+
+const Logo = props => {
+  const {t} = useTranslation();
+  const {title} = props;
+  const src = title ? logoWithText : logo;
+  return (
+    <Image
+      src={src}
+      alt={t('logo-alt')}
+      className="d-block"
+      {...props}
+    />
+  )
+};
+
+Logo.defaultProps = {
+  title: true
+};
+
+export default Logo;
