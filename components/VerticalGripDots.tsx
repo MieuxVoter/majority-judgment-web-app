ba1910524ed6e6561e5b73520a7df34fdbbff516
Théo Sabattié <theo.sabattie@gmail.com>
2025-06-01 12:46:09 +0200

    Fixed: Invalid DOM property fill-opacity. Did you mean fillOpacity (VerticalGripDots)

--- components/VerticalGripDots.tsx
+++ components/VerticalGripDots.tsx
@@ -1,11 +1,11 @@
 const VerticalGripDots = ({fill = "black", opacity = 0.25}) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="8" cy="12" r="2" fill={fill} fill-opacity={opacity} />
-    <circle cx="8" cy="22" r="2" fill={fill} fill-opacity={opacity} />
-    <circle cx="8" cy="2" r="2" fill={fill} fill-opacity={opacity} />
-    <circle cx="16" cy="12" r="2" fill={fill} fill-opacity={opacity} />
-    <circle cx="16" cy="22" r="2" fill={fill} fill-opacity={opacity} />
-    <circle cx="16" cy="2" r="2" fill={fill} fill-opacity={opacity} />
+    <circle cx="8" cy="12" r="2" fill={fill} fillOpacity={opacity} />
+    <circle cx="8" cy="22" r="2" fill={fill} fillOpacity={opacity} />
+    <circle cx="8" cy="2" r="2" fill={fill} fillOpacity={opacity} />
+    <circle cx="16" cy="12" r="2" fill={fill} fillOpacity={opacity} />
+    <circle cx="16" cy="22" r="2" fill={fill} fillOpacity={opacity} />
+    <circle cx="16" cy="2" r="2" fill={fill} fillOpacity={opacity} />
   </svg>
 );
 
